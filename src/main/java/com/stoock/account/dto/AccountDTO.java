5b3f9268229fa40b40269b72abfcc3c82997abbe
TreeBomb-01 <htb010630@gmail.com>
2025-03-17 14:27:20 +0900

    계좌 dto 작성

---
+++ src/main/java/com/stoock/account/dto/AccountDTO.java
@@ -0,0 +1,20 @@
+package com.stoock.account.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class AccountDTO {
+    private String userId;                //유저아이디
+    private String accountNumber;       //계좌번호
+    private String accountPW;
+    private String accountType;         // GENERAL(일반), INVESTMENT(투자)
+    private BigDecimal balance;         // 잔액(소수점 포함 금액 저장)
+    private String status;              // ACTIVE, CLOSED
+    private String currency;            // KRW, USD
+    private String nickname;            // 계좌 별칭
+    private LocalDateTime createdAt;    // 생성 날짜
+    private LocalDateTime updatedAt;    // 업데이트 날짜
+}
