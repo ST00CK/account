33800d718e0e8140d7ce0759683be98e802264f3
TreeBomb-01 <htb010630@gmail.com>
2025-03-17 11:16:18 +0900

    Initial commit

---
+++ src/test/java/com/stoock/account/AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stoock.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
